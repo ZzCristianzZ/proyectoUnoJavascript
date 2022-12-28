55db05b92409172133ba647b4d3ce539a24bf2ef
ZzCristianzZ <crisfegomez@gmail.com>
2022-12-28 18:47:14 -0500

    Fundamentos

--- Fundamentos.js
+++ Fundamentos.js
@@ -1,3 +1,9 @@
-// EL METODO DE EXPONENCIACION (**)
+// ---  EL METODO DE EXPONENCIACION (**)
 
 // En vez de utilizar el  **, utiliza Math.pow(base,exponente);  
+
+// --- EL METODO PARSE FLOAT 
+
+// parseFloat (TipodeDatoaConvertiraNumerico)
+
+1.26.17
