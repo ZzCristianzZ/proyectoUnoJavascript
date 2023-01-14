55a98b974b780c7b82530926be865e52f7ec649f
ZzCristianzZ <crisfegomez@gmail.com>
2023-01-14 18:19:28 -0500

    informacion

---
+++ Calculadora/script.js
@@ -0,0 +1,15 @@
+alert("Funciona Javascript")
+
+const display = document.querySelector("#display")
+const buttons = document.querySelectorAll("button")
+
+buttons.forEach((item)=>{
+    item.onclick = () => {
+        if (item.id == "clear"){
+            display.innerText = ""
+        }else if(item.id == "backspace"){
+            let string = display.innerText.toString()
+            display.innerText = string.substr(0,string.length-1)
+        }
+    }
+})
