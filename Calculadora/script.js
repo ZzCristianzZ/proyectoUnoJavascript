fbb86fd91491ab6cb47b09242c34c9879b39af07
ZzCristianzZ <crisfegomez@gmail.com>
2023-02-08 00:51:28 -0500

    Se agragaron las ultimas lineas de codigo funcinal en el archivo script.js, se corrigio una posicion del icono que que nos permitira cambiar al modo oscuro de la calculadora, y asi mismo falta colocar el logo del favicon y subir nuestro proyecto

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -22,3 +22,13 @@ buttons.forEach((item)=>{
     };
 });
 
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = doc.querySelector("toggler-icon");
+
+let isDark=true;
+themeToggleBtn.onclick=()=>{
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active");
+    isDark = !isDark;
+}
